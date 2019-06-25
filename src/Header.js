3ec1376766b0b0d3d9a204ec7396f2049cad5188
Marianna Vladimirova <marianna.vl.vladimirova@gmail.com>
2019-06-25 23:40:32 +0300

    Add nav

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import {makeStyles} from '@material-ui/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,9 +6,11 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Navigation from './Navigation';
 
 const useStyles = makeStyles({
     root: {
+        display: 'flex',
         flexGrow: 1,
     },
     menuButton: {
@@ -20,7 +22,7 @@ const useStyles = makeStyles({
     loginButton: {
         position: 'absolute',
         right: 0,
-        paddingRight: 24
+        marginRight: 24
     },
     toolBar: {
         backgroundColor: '#056571'
@@ -29,18 +31,29 @@ const useStyles = makeStyles({
 
 export default function Header(){
     const classes = useStyles();
+    const [open, setOpen] = useState();
+
+    function handleOpenNav() {
+        setOpen(true);
+    }
+
+    function handleCloseNav() {
+        setOpen(false);
+    }
+
     return (
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar className={classes.toolBar}>
                     <IconButton edge="start" className={classes.menuButton}
-                    color="inherit" aria-label="Menu">
+                    color="inherit" aria-label="Menu" onClick={handleOpenNav}>
                         <MenuIcon/>
                     </IconButton>
                     <Typography variant="h6">Home</Typography>
                     <Button className={classes.loginButton} color="inherit">Login/Register</Button>
                 </Toolbar>
             </AppBar>
+            <Navigation open={open} handleClose={handleCloseNav}></Navigation>
         </div>
     )
 }
